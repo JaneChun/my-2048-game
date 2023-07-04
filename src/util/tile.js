53d1d9259e2001e87871f5c589357c31e874b307
JaneChun <janechun22@gmail.com>
2023-07-04 23:12:43 +0900

    feat: merge tile function

--- src/util/tile.js
+++ src/util/tile.js
@@ -35,21 +35,23 @@ export function slideTile(x, y, { tileList, setTileList }) {
 	const isVerticalMove = y !== 0;
 	const isMinus = x < 0 || y < 0; // true라면 위로 이동 또는 왼쪽 이동
 
-	const sortedTileList = [...tileList].sort((tile1, tile2) => {
-		// 수직으로 이동했다면 x를 기준으로 정렬, 수평으로 움직였다면 y를 기준으로 정렬
-		const res = isVerticalMove ? tile1.x - tile2.x : tile1.y - tile2.y;
+	const sortedTileList = [...tileList]
+		.map((item) => ({ ...item, isMerged: false, isNew: false }))
+		.sort((tile1, tile2) => {
+			// 수직으로 이동했다면 x를 기준으로 정렬, 수평으로 움직였다면 y를 기준으로 정렬
+			const res = isVerticalMove ? tile1.x - tile2.x : tile1.y - tile2.y;
 
-		if (res !== 0) {
-			return res;
-		} else {
-			// 수직이동인데 같은 줄에 있을 때, 수평 이동인데 같은 줄에 있을 때
-			if (isVerticalMove) {
-				return isMinus ? tile1.y - tile2.y : tile2.y - tile1.y; // 위로 이동이라면 오름차순, 아래로 이동이라면 내림차순
+			if (res !== 0) {
+				return res;
 			} else {
-				return isMinus ? tile1.x - tile2.x : tile2.x - tile2.x; // 왼쪽 이동이라면 오름차순, 오른쪽 이동이라면 내림차순
+				// 수직이동인데 같은 줄에 있을 때, 수평 이동인데 같은 줄에 있을 때
+				if (isVerticalMove) {
+					return isMinus ? tile1.y - tile2.y : tile2.y - tile1.y; // 위로 이동이라면 오름차순, 아래로 이동이라면 내림차순
+				} else {
+					return isMinus ? tile1.x - tile2.x : tile2.x - tile2.x; // 왼쪽 이동이라면 오름차순, 오른쪽 이동이라면 내림차순
+				}
 			}
-		}
-	});
+		});
 
 	let initialPosition = isMinus ? 0 : 3; // 위나 왼쪽으로 이동했으면 ? 맨 위 혹은 맨 왼쪽 0 : 맨 아래 혹은 맨 오른쪽 3
 	let position = initialPosition;
@@ -72,7 +74,43 @@ export function slideTile(x, y, { tileList, setTileList }) {
 			}
 		}
 	}
-	console.log('sortedTileList', sortedTileList);
 
-	setTileList(sortedTileList);
+	let nextPosition = 0; // 타일을 이동하면서 다음 타일의 위치를 설정하기 위해 사용될 변수
+	const newTileList = [...sortedTileList];
+
+	for (let i = 0; i < newTileList.length; i++) {
+		const currentTile = newTileList[i];
+		const nextTile = newTileList[i + 1];
+		const isOnSameLine = isVerticalMove ? currentTile.x === nextTile?.x : currentTile.y === nextTile?.y;
+
+		if (currentTile.isDisabled) continue;
+
+		// nextPosition이 0이 아니고, 현재 타일과 이전 타일이 같은 행/열에 위치한다면
+		if (nextPosition && isOnSameLine) {
+			if (isVerticalMove) {
+				currentTile.y = nextPosition; // 현재 타일의 y좌표를 nextPosition으로 설정한다.
+			} else {
+				currentTile.x = nextPosition;
+			}
+			isMinus ? (nextPosition += 1) : (nextPosition -= 1);
+		} else {
+			nextPosition = 0; // 현재 타일과 이전 타일이 같은 줄에 있지 않으면, nextPosition 변수를 0으로 재설정한다.
+		}
+		// 즉, nextPosition 변수는 이전 타일과 현재 타일이 같은 위치에 있을 때, 병합된 타일의 다음 위치를 결정한다.
+
+		const isMergable = currentTile.value === nextTile?.value && isVerticalMove ? currentTile.x === nextTile?.x : currentTile.y === nextTile?.y;
+
+		if (isMergable) {
+			const tile = makeTile(newTileList);
+			tile.x = currentTile.x;
+			tile.y = currentTile.y;
+			tile.value = currentTile.value * 2;
+			currentTile.isDisabled = true;
+			nextTile.isDisabled = true;
+			newTileList.push(tile);
+		}
+	}
+
+	setTileList(newTileList.filter((item) => !item.isDisabled));
+	console.log(newTileList);
 }
